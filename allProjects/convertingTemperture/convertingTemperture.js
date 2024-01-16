296be51921b4118f06ef051f416972418235b702
min <minchingkao1@gmail.com>
2024-01-16 19:36:09 +0000

    fixing NaN

--- allProjects/convertingTemperture/convertingTemperture.js
+++ allProjects/convertingTemperture/convertingTemperture.js
@@ -1,12 +1,12 @@
 function convertingToCelcus () {
     const inputValue = document.getElementById("inputFahrenheit").value;
     let totalCelciusDegrees = (inputValue - 32) * 5/9 + " °C";
-    document.getElementById("displayCelcius").innerHTML = totalCelciusDegrees;
+    document.getElementById("displayCelcius").innerHTML = Math.trunc(parseFloat(totalCelciusDegrees));
 }
 
 function convertingToFahrenheit () {
     const inputValue = document.getElementById("inputCelcius").value;
     let totalFahrenheitDegrees = ((inputValue * 9/5) + 32) + " °F";
-    document.getElementById("displayFahrenheit").innerHTML = totalFahrenheitDegrees;
+    document.getElementById("displayFahrenheit").innerHTML = Math.trunc(parseFloat(totalFahrenheitDegrees));
 
 }
